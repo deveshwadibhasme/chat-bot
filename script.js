0b0a42d85ec8caffea5d06541d854b4aabacfa3f
Devesh Wadibhasme <146552994+deveshwadibhasme@users.noreply.github.com>
2025-01-30 11:30:06 +0530

    feature/add

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 import GEMINI from './GEMINI_API.js'
 
 const message = prompt('Write Your Prompt Here !!')
+const container = document.querySelector('.container')
 
 const APIURL = GEMINI.API + '?key=' + GEMINI.KEY;
 
@@ -8,7 +9,7 @@ const generateAIResponse = async (prompt) => {
     const headers = {
         'Content-Type': 'application/json',
     };
-    
+
     const body = JSON.stringify({
         contents: [{
             parts: [{
@@ -22,7 +23,23 @@ const generateAIResponse = async (prompt) => {
         body: body
     })
     const data = await response.json()
-    console.log(data.candidates[0].content.parts[0].text);
+    // console.log(data.candidates[0].content.parts[0].text);
+    try{
+    cleanUp(data.candidates[0].content.parts[0].text)
+    }
+    catch{
+        container.innerHTML = '<b>API Working Slow...</b> Try Again....'
+    }
 
 }
 generateAIResponse(message)
+
+function cleanUp(data) {
+    const cleanData = data
+        .replace(/\* \*\*/g, '<br><b>')
+        .replace(/\*\*/g, '</b>')
+        .replace(/\./g, '.<br>')
+        .replace(/\:/g, ' :<br>')
+        
+    container.innerHTML = cleanData
+}
