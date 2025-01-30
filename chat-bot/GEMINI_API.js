243567a5d87031c5816b109ee5376613333b70f5
Devesh Wadibhasme <146552994+deveshwadibhasme@users.noreply.github.com>
2025-01-30 10:54:36 +0530

    basic-coding

---
+++ chat-bot/GEMINI_API.js
@@ -0,0 +1,6 @@
+const GEMINI = {
+    "API":"https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent",
+    "KEY":"AIzaSyCCtP9YC-XV4uhdyEKV5DoNySiVkTtobEU"
+}
+
+export default GEMINI
