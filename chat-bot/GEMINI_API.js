2d5e605f04237110ddaf124163d59e792cb6e533
Devesh Wadibhasme <146552994+deveshwadibhasme@users.noreply.github.com>
2025-01-30 10:55:08 +0530

    Delete chat-bot directory

--- chat-bot/GEMINI_API.js
+++
@@ -1,6 +0,0 @@
-const GEMINI = {
-    "API":"https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent",
-    "KEY":"AIzaSyCCtP9YC-XV4uhdyEKV5DoNySiVkTtobEU"
-}
-
-export default GEMINI
